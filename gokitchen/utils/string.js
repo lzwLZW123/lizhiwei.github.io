fd2a875624a662a8feefc00c8f9c1ff5334da77f
lzwLZW123 <>
2020-03-26 21:59:46 +0800

    111

--- gokitchen/utils/string.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    stringify: function(n) {
-        var o = [];
-        for (var e in n) o.push(encodeURIComponent(e) + "=" + encodeURIComponent(n[e]));
-        return o.join("&");
-    },
-    parse: function() {}
-};
